6e44547b86312b31b1ce753cd867d4bc2272dbf4
hlin <Zxsaqw12@&$>
2020-03-01 16:50:16 -0500

    Done Lab

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,45 +1,63 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    private Map<String, List<String>> map;
+    List<String> numbers;
+
     public PhoneBook(Map<String, List<String>> map) {
+        this.map = new LinkedHashMap<>(map.size());
     }
 
     public PhoneBook() {
-        this(null);
+        this.map = new LinkedHashMap<>();
+        this.numbers = new ArrayList<>();
     }
 
     public void add(String name, String phoneNumber) {
+        numbers.add(phoneNumber);
+        map.put(name, numbers);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        numbers.addAll(Arrays.asList(phoneNumbers));
+        map.put(name, numbers);
     }
 
     public void remove(String name) {
+        map.remove(name);
     }
 
-    public Boolean hasEntry(String name) {
-        return null;
+    public Boolean hasEntry(String nameOrPhoneNumber) {
+        return map.containsKey(nameOrPhoneNumber) || this.numbers.contains(nameOrPhoneNumber);
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return map.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
-        return null;
+        String result = "";
+        for (Map.Entry<String, List<String>> entry: map.entrySet())
+            if (entry.getValue().contains(phoneNumber)){
+                result = entry.getKey();
+            }
+         return result;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        List<String> names = new ArrayList<>();
+        for (Map.Entry<String, List<String>> entry : map.entrySet())
+            names.add(entry.getKey());
+        return names;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return this.map;
     }
 }
